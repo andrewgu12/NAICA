ae62ee2584152f21cbbb876954f4740d1958274d
Andrew Gu <andrew@mingbogu.com>
2016-08-17 00:01:19 -0400

    event deletion done

--- public/javascript/library.js
+++ public/javascript/library.js
@@ -5,20 +5,18 @@ $(document).ready(function() {
   // adjust homepage height based on background image size
   $('#homepage-container').css('height', $(document).height() - $('.top-bar').height());
 
-
   // admin events modal
   $('.edit_event').on('click', function(e){
     e.preventDefault();
     var id = $(this).parent().parent().attr('id');
     $.ajax({
       type: 'GET',
-      url: '/admin/events/'+id,
+      url: '/admin/events/edit/'+id,
     }).done(function(data) {
       var eventOrganizer = (data.naicaEvent) ? 'NAICA' : 'other';
-      $('input#event_id').text(data._id);
-      $('input#event_title').val(data.description);
+      $('input#event_id').val(data._id);
+      $('input#event_title').val(data.title);
       $('input#event_location').val(data.location);
-      console.log(data.dateStamp);
       $('input#event_date').val(data.dateStamp);
       $('select#event_group').val(eventOrganizer);
       $('input#event_time_start').val(data.eventTimeStart);
@@ -27,7 +25,23 @@ $(document).ready(function() {
       $('input#event_more_info').val(data.moreInfo);
       $('textarea#event_description').val(data.description);
       $('#edit_modal').foundation('open');
-      console.log(data);
     });
+  });
+
+  // throw up confirm modal to make sure they actually want to delete
+  $('.delete_event').on('click', function(e) {
+    var parentRow  = $(this).parent().parent(),
+        eventID    = $(parentRow).attr('id'),
+        eventTitle = $($(parentRow).find('td')[0]).text(),
+        deleteLink = '/admin/events/delete/' + eventID;
+
+    $('p.confirm_message').text('Are you sure you want to delete (' + eventTitle + ')?');
+    $('a.delete-event-modal').attr('href', deleteLink);
+    $('#delete_modal').foundation('open');
+  });
+
+  // close reveal modal
+  $('.close-reveal-modal').on('click', function() {
+    $('#delete_modal').foundation('close');
   });
 });
--- routes/admin.js
+++ routes/admin.js
@@ -32,12 +32,13 @@ router.get('/login', (req, res, next) => {
 **********/
 
 router.get('/events/', isLoggedIn, (req, res, next) => {
+  const message = (req.session.message) ? req.session.message : null;
   Event.find((err, events) => {
     if (err)
       throw err;
 
     const eventsTable = ReactDOMServer.renderToString(React.createElement(EventsTable, {events: events}));
-    res.render('admin/events/view_all', {title: 'View All Events | NAICA', eventsTable: eventsTable});
+    res.render('admin/events/view_all', {title: 'View All Events | NAICA', eventsTable: eventsTable, message: message});
   }).where('eventDate').sort({eventDate: 1});
 });
 
@@ -56,7 +57,7 @@ router.post('/events/add', isLoggedIn, (req, res, next) => {
         moreInfo    = req.body.event_more_info,
         description = req.body.event_description,
         naicaEvent  = (req.body.event_group === 'NAICA');
-  console.log(req.body.event_date);
+
   // create new event object
   const event = new Event({
     eventDate:      dateStamp,
@@ -82,12 +83,61 @@ router.get('/events/success', isLoggedIn, (req, res, next) => {
   res.render('admin/events/add_success', {title: 'Event Added Succesfully | NAICA'});
 });
 
-router.get('/events/:id', isLoggedIn, (req, res, next) => {
+// get the event to edit
+router.get('/events/edit/:id', isLoggedIn, (req, res, next) => {
   Event.findOne({'_id': req.params.id}, (err, event) => {
     event['dateStamp'] = moment.unix(event.eventDate).format('YYYY-MM-DD');
-    console.log(event);
     res.send(event);
   }).lean();
+});
+
+// find event and update
+router.post('/events/edit/', isLoggedIn, (req, res, next) => {
+  const eventID        = req.body.event_id,
+        name           = req.body.event_title,
+        location       = req.body.event_location,
+        naicaEvent     = (req.body.event_group === 'NAICA'),
+        dateStamp      = moment(req.body.event_date).unix(),
+        eventTimeStart = req.body.event_time_start,
+        eventTimeEnd   = req.body.event_time_end,
+        rsvpLink       = req.body.event_rsvp_link,
+        moreInfo       = req.body.event_more_info,
+        description    = req.body.event_description;
+
+  Event.findById(eventID, (err, event) => {
+    if (err)
+      throw err;
+
+    // update all fields
+    event.title          = name;
+    event.location       = location;
+    event.naicaEvent     = naicaEvent;
+    event.eventDate      = dateStamp;
+    event.eventTimeStart = eventTimeStart;
+    event.eventTimeEnd   = eventTimeEnd;
+    event.rsvpLink       = rsvpLink;
+    event.moreInfo       = moreInfo;
+    event.description    = description;
+
+    // save the event
+    event.save((err, savedEvent) => {
+      if (err)
+        throw err;
+
+      req.session.message = `The event (${savedEvent.title}) was updated!`;
+      res.redirect('/admin/events');
+    });
+  });
+});
+
+// delete event
+router.get('/events/delete/:id', isLoggedIn, (req, res, next) => {
+  const eventID = req.params.id;
+
+  Event.findByIdAndRemove(eventID, (err, event) => {
+    req.session.message = `The event (${event.title}) was removed!`;
+    res.redirect('/admin/events');
+  });
 });
 
 /**************************
