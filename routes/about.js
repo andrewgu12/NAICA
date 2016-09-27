f584cd53ffe321b8e5da0386218be68a2961aeb5
Chang Chen <cchen679@gmail.com>
2016-09-26 22:00:06 -0400

    edit about page with profiles and text

--- routes/about.js
+++ routes/about.js
@@ -12,25 +12,31 @@ router.get('/', (req, res, next) => {
   let testDirector = new Director({
     name: 'Test Director',
     description: 'Test Description',
-    imgSource: '/images/logo_red.png',
+    imgSource: '/images/profiles/cchen.png',
     title: 'Test Title'
   });
   let testDirector2 = new Director({
     name: 'Test Director',
     description: 'Test Description',
-    imgSource: '/images/logo_red.png',
+    imgSource: '/images/profiles/cseto.png',
     title: 'Test Title'
   });
   let testDirector3 = new Director({
     name: 'Test Director',
     description: 'Test Description',
-    imgSource: '/images/logo_red.png',
+    imgSource: '/images/profiles/vyu.png',
     title: 'Test Title'
   });
   let testDirector4 = new Director({
     name: 'Test Director',
     description: 'Test Description',
-    imgSource: '/images/logo_red.png',
+    imgSource: '/images/profiles/dchou.png',
+    title: 'Test Title'
+  });
+  let testDirector5 = new Director({
+    name: 'Test Director',
+    description: 'Test Description',
+    imgSource: '/images/profiles/aliu.png',
     title: 'Test Title'
   });
 
@@ -38,13 +44,15 @@ router.get('/', (req, res, next) => {
   const directorBox2 = ReactDOMServer.renderToString(React.createElement(DirectorBox, {director: testDirector2}));
   const directorBox3 = ReactDOMServer.renderToString(React.createElement(DirectorBox, {director: testDirector3}));
   const directorBox4 = ReactDOMServer.renderToString(React.createElement(DirectorBox, {director: testDirector4}));
+  const directorBox5 = ReactDOMServer.renderToString(React.createElement(DirectorBox, {director: testDirector5}));
 
   res.render('about/table', {
     title: 'About Us | NAICA',
     director1: directorBox1,
     director2: directorBox2,
     director3: directorBox3,
-    director4: directorBox4
+    director4: directorBox4,
+    director5: directorBox5
   });
 });
 
